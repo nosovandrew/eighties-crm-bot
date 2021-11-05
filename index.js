ab3d675d59fec32e28e67bdb6f3f5227b724d198
Andrew Nosov <andrewnosov@outlook.com>
2021-11-05 14:47:20 +0500

    add more data and style msg from bot (testing)

--- index.js
+++ index.js
@@ -7,32 +7,43 @@ const port = process.env.PORT || 3000;
 const app = express(); // create express server
 
 const bot = new Telegraf(process.env.TOKEN); // create Telegram bot
+bot.startPolling(); // start poll updates
 
+// use middleware (setting headers)
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept'
     );
-    next();
+    next(); // go to the next middleware
 });
 
 app.get('/', (req, res) => {
     res.send('Hello, I am CRM bot!');
-})
+});
 
-app.use(express.json());
+app.use(express.json()); // parse incoming data
 
+// handle post query from ecommerce app
 app.post('/', (req, res) => {
-    console.log(req);
+    // send msg (new order info) to Admin
     bot.use((ctx) => {
         ctx.telegram.sendMessage(
             process.env.ADMIN_TG_ID,
-            `Msg: ${req.body.text}`
+            `New order received!
+            Contact number: ${req.body.phoneNumber}
+            Shipping:
+            ${req.body.firstName} ${req.body.lastName}
+            address: ${req.body.address}
+            postalCode: ${req.body.postalCode}
+            Items: ${req.body.items.map(_item => {
+                return `${_item.item}\n${_item.sku}\n${_item.qtyForSale}\n`;
+            })}
+            Total: ${req.body.total} ${req.body.currency}`
         );
     });
 });
 
-bot.startPolling(); // run Telegram bot
-
+// start express server
 app.listen(port, () => console.log(`Bot started on port ${port}.`));
