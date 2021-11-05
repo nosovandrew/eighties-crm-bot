ab0c8f7c9917a80edb61ea44e53578fd0e2c7d0d
Andrew Nosov <andrewnosov@outlook.com>
2021-11-05 17:08:12 +0500

    complete mvp

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import { Telegraf } from 'telegraf';
+import { Telegram } from 'telegraf';
 // import fetch from 'node-fetch';
 const port = process.env.PORT || 3000;
 const app = express(); // create express server
 
-// Telegram bot settings
-const config = {
-	telegram: { 
-        agent: null,
-        webhookReply: true,
-    }
-};
+// global context for sending msg by request from app
+const telegram = new Telegram(process.env.TOKEN, {
+    agent: null,
+    webhookReply: true,
+});
 
-const bot = new Telegraf(process.env.TOKEN, config); // create Telegram bot
+const bot = new Telegraf(process.env.TOKEN); // create Telegram bot
+bot.startPolling(); // start poll updates
 
 // use middleware (setting headers)
 app.use((req, res, next) => {
@@ -33,31 +33,28 @@ app.get('/', (req, res) => {
 app.use(express.json()); // parse incoming data
 
 // handle post query from ecommerce app
-app.post('/', (req, res) => {
+app.post('/send-message', (req, res) => {
     try {
         // send msg (new order info) to Admin
-        bot.use((ctx) => {
-            ctx.telegram.sendMessage(
-                process.env.ADMIN_TG_ID,
-                `New order received!\n
-                Contact number: ${req.body.phoneNumber}\n\n
-                Shipping:\n
-                ${req.body.firstName} ${req.body.lastName}\n
-                address: ${req.body.address}\n
-                postalCode: ${req.body.postalCode}\n\n
-                Items:\n ${req.body.items.map((_item) => {
-                    return `${_item.item}\n${_item.sku}\n${_item.qtyForSale}\n\n`;
-                })}
-                Total: ${req.body.total} ${req.body.currency}`
-            );
-        });
+        telegram.sendMessage(
+            process.env.ADMIN_TG_ID,
+            `New order received!\n
+            *Contact number*: ${req.body.phoneNumber}\n
+            Shipping:\n
+            ${req.body.firstName} ${req.body.lastName}\n
+            address: ${req.body.address}\n
+            postalCode: ${req.body.postalCode}\n\n
+            Items:\n ${req.body.items.map(
+                (_item) =>
+                    `Name: ${_item.item}\nSKU: ${_item.sku}\nQTY: ${_item.qtyForSale}\n`
+            )}
+            Total: ${req.body.total} ${req.body.currency}`
+        );
         return res.status(200).json({ msg: 'Order sended to admin!' });
     } catch (error) {
         return res.status(400).json({ error: 'Something wrong...' });
     }
 });
-
-bot.startPolling(); // start poll updates
 
 // start express server
 app.listen(port, () => console.log(`Bot started on port ${port}.`));
